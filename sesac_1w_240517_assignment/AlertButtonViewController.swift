f29e3a63f983803e21b10f2a8ce99aabb80df32c
hankyeol-dev <ddovlek@kakao.com>
2024-05-22 20:23:37 +0900

    [240522 - UserDefaults 연습] - viewDidLoad 메서드 내부에서 UserDefaults에 저장된 값 조회 후 반영 - 클릭 이벤트로 UserDefaults에 해당하는 버튼의 tag를 키로 잡아서 값 저장

---
+++ sesac_1w_240517_assignment/AlertButtonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlertButtonViewController.swift
+//  sesac_1w_240517_assignment
+//
+//  Created by 강한결 on 5/22/24.
+//
+
+import UIKit
+
+class AlertButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sesac_1w_240517_assignment/EmotionDiaryViewController.swift
+++ sesac_1w_240517_assignment/EmotionDiaryViewController.swift
@@ -12,18 +12,19 @@ class EmotionDiaryViewController: UIViewController {
     @IBOutlet var imageButtons: [UIButton]!
     @IBOutlet var imageLabels: [UILabel]!
     
-    var labelCounts: Array<Int> = [0,0,0,0,0,0,0,0,0]
+    var labelCounts: Array<Int> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageLabels.forEach { el in
-            labelStyling(el, title: "감정")
+        imageButtons.forEach { el in
+            labelCounts.append(UserDefaults.standard.integer(forKey: "\(el.tag)"))
+            labelStyling(imageLabels[el.tag], title: "감정", labelTag: el.tag)
         }
     }
     
-    func labelStyling(_ label: UILabel, title labelTitle: String) {
-        label.text = labelTitle + ": 0"
+    func labelStyling(_ label: UILabel, title labelTitle: String, labelTag tag: Int) {
+        label.text = labelTitle + ": \(labelCounts[tag])"
         label.textAlignment = .center
         label.font = .boldSystemFont(ofSize: 13)
     }
@@ -31,5 +32,6 @@ class EmotionDiaryViewController: UIViewController {
     @IBAction func touchCountUp(_ sender: UIButton) {
         labelCounts[sender.tag] += 1
         imageLabels[sender.tag].text = "감정: " + "\(labelCounts[sender.tag])"
+        UserDefaults.standard.set(labelCounts[sender.tag], forKey: "\(sender.tag)")
     }
 }
